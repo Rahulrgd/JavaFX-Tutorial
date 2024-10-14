1591fd0c32d0ff70d5121dbd0edbf92730e65a56
RahulGupta <rahulrgd21@gmail.com>
2024-10-14 18:32:05 +0530

    Completed Tutorial - Lambda expression

--- buttonexplorer/src/main/java/com/buttonexplorer/App.java
+++ buttonexplorer/src/main/java/com/buttonexplorer/App.java
@@ -1,5 +1,6 @@
 package com.buttonexplorer;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -14,42 +15,31 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 /**
  * JavaFX App
  */
-public class App extends Application{
+public class App extends Application {
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        BorderPane root = new BorderPane();
+  @Override
+  public void start(Stage stage) throws IOException {
+    BorderPane root = new BorderPane();
 
-        Image image = new Image(getClass().getResourceAsStream("icon.png"));
-        ImageView iView = new ImageView(image);
-        Button button = new Button("Sa_ve", iView);
-        button.setFont(Font.font("Roboto", FontWeight.BOLD, 18));
-        // button.setRotate(90);
+    Image image = new Image(getClass().getResourceAsStream("icon.png"));
+    ImageView iView = new ImageView(image);
+    Button button = new Button("Sa_ve", iView);
+    button.setFont(Font.font("Roboto", FontWeight.BOLD, 18));
+    // button.setRotate(90);
 
-        button.setOnAction(new EventHandler<ActionEvent>() {
+    button.setOnAction(e -> System.out.println("Save button pressed!"));
 
-            @Override
-            public void handle(ActionEvent arg0) {
-                System.out.println("Save button pressed!");
-            }
-            
-        });
+    root.setCenter(button);
 
+    Scene scene = new Scene(root, 500, 500);
+    stage.setScene(scene);
+    stage.show();
+  }
 
-        root.setCenter(button);
-
-        Scene scene = new Scene(root, 500, 500);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
-
+  public static void main(String[] args) {
+    launch();
+  }
 }
